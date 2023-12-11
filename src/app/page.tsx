b35a1259da3564f64921f23f82a9acf4e91b0693
Valeria Gora <v.gora@watched.tech>
2023-12-11 15:55:35 +0200

    md/lg cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,59 @@
 "use client";
 import { ReactECharts, ReactEChartsProps } from "@/components/ReactECharts";
-import { lgOption, smOption } from "@/data";
+import { lgOption, mdOption, smOption } from "@/data";
 import { styled } from "@mui/material";
-import { useEffect } from "react";
 import styles from "./page.module.css";
+
 // console.log("lgOption", lgOption.series.data.length);
+
 const LgChart = styled("div")<{ height?: number }>(({ height }) => ({
-  border: "1px solid #ddd",
+  // border: "1px solid #ddd",
   // height,
+  position: "relative",
   height: height < 120 ? "120px" : height,
 }));
 
+const Image = ({ url }) => {
+  return (
+    <div
+      style={{
+        position: "absolute",
+        right: 0,
+        top: "50%",
+        transform: "translateY(-60px)",
+        backgroundImage: `url(${url})`,
+        backgroundSize: "contain",
+        backgroundPosition: "center center",
+        backgroundRepeat: "no-repeat",
+        backgroundColor: "#1A1A25",
+        // border: "1px solid pink",
+        width: 120,
+        height: 120,
+      }}
+    />
+  );
+};
+
 const SmallCard = ({ title, children }: any) => {
   return (
-    <div className={styles.main}>
+    <div className={styles.sm}>
       <h3 className={styles.title}>
         Which words would you use to describe the TV promo?
       </h3>
       <div className={styles.smChart}>{children}</div>
+    </div>
+  );
+};
+const MediumCard = ({ title, children }: any) => {
+  return (
+    <div className={styles.md}>
+      <h3 className={styles.title}>
+        Which words would you use to describe the TV promo?
+      </h3>
+      <div className={styles.mdChart}>
+        {children}
+        <Image url="https://images.unsplash.com/photo-1702068213563-2d29f10639ec?q=80&w=2048&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+      </div>
     </div>
   );
 };
@@ -29,6 +65,7 @@ const LargeCard = ({ title, children }: any) => {
       </h3>
       <LgChart height={lgOption.series.data.length * 24 + 20}>
         {children}
+        <Image url="https://images.unsplash.com/photo-1702068213563-2d29f10639ec?q=80&w=2048&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
       </LgChart>
     </div>
   );
@@ -39,6 +76,9 @@ export default function Home() {
       <SmallCard>
         <ReactECharts option={smOption} />
       </SmallCard>
+      <MediumCard>
+        <ReactECharts option={mdOption} />
+      </MediumCard>
       <LargeCard>
         <ReactECharts option={lgOption} />
       </LargeCard>
--- src/data/index.ts
+++ src/data/index.ts
@@ -1,7 +1,7 @@
 import { ReactEChartsProps } from "@/components/ReactECharts";
 
 export const data = [
-  { value: 10, name: "Search Engine" },
+  { value: 10, name: "Search Engine Search 123" },
   { value: 20, name: "Direct" },
   { value: 25, name: "Other" },
   { value: 15, name: "Option 1" },
@@ -17,28 +17,37 @@ export const data = [
 export const lgData = [
   {
     value: 10,
-    name: "1",
-    // name: "Search Engine Search Engine Search Engine Search Engine",
+    name: "Search Engine Search Engine Search Engine Search Engine",
   },
-  { value: 20, name: "Direct" },
+  { value: 35, name: "Direct" },
   { value: 25, name: "Other" },
   { value: 15, name: "Option 1" },
   { value: 2, name: "Option 2" },
   { value: 3, name: "Option 3" },
-  { value: 2, name: "Option 4" },
-  { value: 3, name: "Option 5" },
-  { value: 5, name: "Option 6" },
-  { value: 5, name: "Option 7" },
-  { value: 5, name: "Option 8" },
-  // { value: 5, name: "Option 9" },
+  { value: 4, name: "Option 4" },
+  { value: 3, name: "Option 5 Engine Search Engine Search Engine Search" },
+  { value: 2, name: "Option 6" },
+  { value: 2, name: "Option 7" },
+  { value: 2, name: "Option 8" },
+  { value: 2, name: "Option 9" },
+  { value: 2, name: "Option 10" },
+  { value: 2, name: "Option 11" },
+  { value: 1, name: "Option 12" },
 ];
 export const smOption: ReactEChartsProps["option"] = {
   tooltip: {
     trigger: "item",
   },
+  grid: {
+    // show: true,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    left: "124px",
+  },
   series: {
     color: ["#FF877C", "#25B4C8", "#D88ADA", "#9ECC7F", "#F9DE82", "#39519B"],
-    center: ["23%", "50%"],
+    center: ["60px", "50%"],
     type: "pie",
     radius: ["60%", "90%"],
     data,
@@ -51,78 +60,19 @@ export const smOption: ReactEChartsProps["option"] = {
     pageIconColor: "#3B3E4A",
     pageIconInactiveColor: "#2F313B",
     pageIconSize: 10,
-    // pageTextStyle: {},
+
     formatter: (name: string) => {
       const percents = data.find((item) => item.name === name)?.value;
       const label = `{value|${percents}%} {name|${name}}`;
       const overflowed = `{value|${percents}%} {name|${
         name.slice(0, 11) + "..."
       }}`;
+      // return label;
       return name.length < 10 ? label : overflowed;
     },
     textStyle: {
-      fontFamily: "Manrope, sans-serif",
-      rich: {
-        value: {
-          color: "#fff",
-          fontSize: 14,
-        },
-        name: {
-          color: "#6C7080",
-          fontSize: 14,
-        },
-      },
-    },
-    icon: "circle",
-    top: "0px",
-    right: "1%",
-    width: "20%",
-    orient: "vertical",
-    inactiveColor: "#333",
-  },
-};
-//
-//
-//
-export const lgOption: ReactEChartsProps["option"] = {
-  tooltip: {
-    trigger: "item",
-  },
-  series: {
-    // width: 472,
-    color: ["#FF877C", "#25B4C8", "#D88ADA", "#9ECC7F", "#F9DE82", "#39519B"],
-    center: ["23%", "50%"],
-    type: "pie",
-    radius: ["60%", "90%"],
-    data: lgData,
-    label: {
-      show: false,
-    },
-  },
-  legend: {
-    width: 340,
-
-    type: "scroll",
-    pageIconColor: "pink",
-    pageIconInactiveColor: "pink",
-    // pageIconColor: "transparent",
-    // pageIconInactiveColor: "transparent",
-    pageIconSize: 0,
-    pageTextStyle: {
-      fontSize: 1,
-      // display: "none",
-    },
-    // pageButtonGap: 20,
-
-    formatter: (name: string) => {
-      const percents = lgData.find((item) => item.name === name)?.value;
-      const label = `{value|${percents}%} {name|${name}}`;
-      return label;
-    },
-    textStyle: {
-      width: 340,
+      width: 148,
       overflow: "break",
-      //
       fontFamily: "Manrope, sans-serif",
       rich: {
         value: {
@@ -137,9 +87,126 @@ export const lgOption: ReactEChartsProps["option"] = {
     },
     icon: "circle",
     top: "center",
-    // right: "10%",
-    right: "100px",
-    // width: "20%",
+    left: "124px",
+    orient: "vertical",
+    inactiveColor: "#333",
+  },
+};
+//
+//
+//
+export const mdOption: ReactEChartsProps["option"] = {
+  tooltip: {
+    trigger: "item",
+  },
+  series: {
+    color: ["#FF877C", "#25B4C8", "#D88ADA", "#9ECC7F", "#F9DE82", "#39519B"],
+    center: ["25%", "50%"],
+    type: "pie",
+    radius: ["30%", "50%"],
+    data: lgData,
+    label: {
+      show: false,
+    },
+  },
+  grid: {
+    // show: true,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    left: "50%",
+  },
+  legend: {
+    type: "scroll",
+    pageIconColor: "#3B3E4A",
+    pageIconInactiveColor: "#2F313B",
+    pageIconSize: 10,
+
+    formatter: (name: string) => {
+      const percents = lgData.find((item) => item.name === name)?.value;
+      const label = `{value|${percents}%} {name|${name}}`;
+      const overflowed = `{value|${percents}%} {name|${
+        name.slice(0, 14) + "..."
+      }}`;
+      return name.length < 15 ? label : overflowed;
+    },
+    textStyle: {
+      width: 152,
+      overflow: "break",
+      fontFamily: "Manrope, sans-serif",
+      rich: {
+        value: {
+          color: "#fff",
+          fontSize: 14,
+        },
+        name: {
+          color: "#6C7080",
+          fontSize: 14,
+        },
+      },
+    },
+    icon: "circle",
+    top: "center",
+    left: "50%",
+    orient: "vertical",
+    inactiveColor: "#333",
+  },
+};
+export const lgOption: ReactEChartsProps["option"] = {
+  tooltip: {
+    trigger: "item",
+  },
+  series: {
+    color: ["#FF877C", "#25B4C8", "#D88ADA", "#9ECC7F", "#F9DE82", "#39519B"],
+    center: ["25%", "50%"],
+    type: "pie",
+    radius: ["60%", "90%"],
+    data: lgData,
+    label: {
+      show: false,
+    },
+  },
+  grid: {
+    // show: true,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    left: "50%",
+  },
+  legend: {
+    type: "scroll",
+    pageIconColor: "pink",
+    pageIconInactiveColor: "pink",
+    pageIconSize: 0,
+    pageTextStyle: {
+      fontSize: 1,
+    },
+    formatter: (name: string) => {
+      const percents = lgData.find((item) => item.name === name)?.value;
+      const label = `{value|${percents}%} {name|${name}}`;
+      const overflowed = `{value|${percents}%} {name|${
+        name.slice(0, 39) + "..."
+      }}`;
+      return name.length < 40 ? label : overflowed;
+    },
+    textStyle: {
+      width: 340,
+      overflow: "break",
+      fontFamily: "Manrope, sans-serif",
+      rich: {
+        value: {
+          color: "#fff",
+          fontSize: 14,
+        },
+        name: {
+          color: "#6C7080",
+          fontSize: 14,
+        },
+      },
+    },
+    icon: "circle",
+    top: "center",
+    left: "50%",
     orient: "vertical",
     inactiveColor: "#333",
   },
