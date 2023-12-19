c824f8c9e1590f1dacfcbdc6152de09f5000ff1c
Valeria Gora <v.gora@watched.tech>
2023-12-19 18:19:27 +0200

    commi1

--- src/data/bar.ts
+++ src/data/bar.ts
@@ -1,6 +1,19 @@
 import { images } from "@/app/bar/page";
 import { ReactEChartsProps } from "@/components/ReactECharts";
 
+function createSvgDataUrl(imageUrl: string) {
+  const svgString = `
+    <svg xmlns="http://www.w3.org/2000/svg" width="72" height="72">
+      <rect width="100%" height="100%" fill="black" />
+      <image href="${imageUrl}" x="0%" y="0%" height="100%" width="100%" transform="translate(-50%, -50%)" preserveAspectRatio="xMidYMid meet" />
+    </svg>
+  `;
+  // return "path://M 0 0 L 10 5 L 0 10 z";
+  // return "image://https://echarts.apache.org/examples/data/asset/img/weather/sunny_128.png";
+  // return "image://data:image;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAlUlEQVRYw2NgGAWjYBSMgiEAooE4jUwcTQ0H1APxfzJxPTUcwA3ET8mw/ClUL1VAMhkOSKZmOmAG4kskWH4JqoeqwJ0EB7jTKkfsIMLyHbTMkrpA/AeP5X+gamgK5uBxwBx6FEySQPwFi+VfoHJ0AfW0KnTILZyoWuiQUzglMwwAgBVONCl0SCmc3BlGwSgYBaOAAgAAeotyvZwCFhMAAAAASUVORK5CYII=";
+  // return 'image://data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path fill="%23f09f53" d="M4.996 4.996v22.008h22.008L4.996 4.996zm4 9 9.008 9.008H8.996v-9.008z"/><path fill="%231a4875" d="M27.357 26.65 5.35 4.643a.5.5 0 0 0-.854.353v22.008a.5.5 0 0 0 .5.5h22.008a.5.5 0 0 0 .353-.854zm-21.861-.146v-2h.5a.5.5 0 0 0 0-1h-.5v-1h1.5a.5.5 0 0 0 0-1h-1.5v-1h.5a.5.5 0 0 0 0-1h-.5v-1h1.5a.5.5 0 0 0 0-1h-1.5v-1h.5a.5.5 0 0 0 0-1h-.5v-1h1.5a.5.5 0 0 0 0-1h-1.5v-1h.5a.5.5 0 0 0 0-1h-.5v-1h1.5a.5.5 0 0 0 0-1h-1.5V6.203l20.301 20.301H5.496z"/><path fill="%231a4875" d="M8.996 23.504h9.008a.5.5 0 0 0 .354-.854L9.35 13.643a.5.5 0 0 0-.854.354v9.008a.5.5 0 0 0 .5.499zm.5-8.301 7.301 7.301H9.496v-7.301z"/></svg>';
+}
+
 export const getSmOption: ReactEChartsProps["option"] = (
   data: any[],
   hasOverflow: boolean
@@ -56,6 +69,7 @@ export const getSmOption: ReactEChartsProps["option"] = (
   yAxis: {
     axisLabel: {
       margin: 12,
+      show: true,
 
       formatter: (name: string, idx: number) => {
         if (hasOverflow) {
@@ -219,7 +233,8 @@ export const getMdOption: ReactEChartsProps["option"] = (
             width: 72,
             fontSize: 60,
             backgroundColor: {
-              image: images.entertaining,
+              image: createSvgDataUrl(images.entertaining),
+              // image: images.entertaining,
             },
           },
           exciting: {
